95c744463dcd29bab65498f224d5d768af5d1955
DavyAndrade <hallszindementahhh@gmail.com>
2022-11-23 12:31:29 -0300

    Iniciando Home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css'
+
+function Home() {
+    return (
+        <>
+
+            <h1 className='titulo'>Home</h1>
+            <div className='container'>
+                <img src="https://www.kabum.com.br/core/_next/image?url=https://themes.kabum.com.br/conteudo/layout/2944/banner_img.jpg&w=1920&h=400&q=100" alt="Imagem Home" className='img' />
+            </div>
+            <p className='texto'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis ducimus, odio nesciunt quae eum nulla animi est minima repudiandae impedit in, necessitatibus fugiat optio officiis aliquid recusandae modi! Deserunt, explicabo.
+            </p>
+        </>
+    )
+}
+
+export default Home
