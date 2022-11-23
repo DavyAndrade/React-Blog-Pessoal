5c4703c8dd1eba35dc9adc224115e2e69a7fc5d4
DavyAndrade <hallszindementahhh@gmail.com>
2022-11-23 12:36:18 -0300

    Começando o React

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,7 +9,7 @@ function Home() {
             <div className='container'>
                 <img src="https://www.kabum.com.br/core/_next/image?url=https://themes.kabum.com.br/conteudo/layout/2944/banner_img.jpg&w=1920&h=400&q=100" alt="Imagem Home" className='img' />
             </div>
-            <p className='texto'>
+            <p className='texto mt-3'>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis ducimus, odio nesciunt quae eum nulla animi est minima repudiandae impedit in, necessitatibus fugiat optio officiis aliquid recusandae modi! Deserunt, explicabo.
             </p>
         </>
